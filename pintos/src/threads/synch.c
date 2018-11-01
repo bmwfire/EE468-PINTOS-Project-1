0909140500a0184eeb27e09436f6abda5739c7fe
Charles Dickens <charlieadickens@gmail.com>
2018-11-01 07:10:01 -1000

    compile errs 6

--- pintos/src/threads/synch.c
+++ pintos/src/threads/synch.c
@@ -399,8 +399,8 @@ bool lock_priority_compare(const struct list_elem *e_1, const struct list_elem *
   const struct lock *l_1 = list_entry(e_1, struct lock, lock_list_elem);
   const struct lock *l_2 = list_entry(e_2, struct lock, lock_list_elem);
 
-  highest_priority_e1 = list_entry(list_begin(l_1->semaphore.waiters), struct thread, elem)->priority;
-  highest_priority_e2 = list_entry(list_begin(l_2->semaphore.waiters), struct thread, elem)->priority;
+  highest_priority_e1 = list_entry(list_begin(&(l_1->semaphore.waiters)), struct thread, elem)->priority;
+  highest_priority_e2 = list_entry(list_begin(&(l_2->semaphore.waiters)), struct thread, elem)->priority;
 
   return highest_priority_e1 > highest_priority_e2;
 }
